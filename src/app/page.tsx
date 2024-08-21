d1fc4712b4f2541b4dd50f085e23de9d24d51615
tutman96 <11356668+tutman96@users.noreply.github.com>
2024-08-21 18:07:46 -0400

    Remove placeholder site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Image from "next/image";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redirect('/scenes');
-  return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
-      <Image src="/icon.png" width={100} height={100} alt="logo" />
-      <Typography variant="h2">Coming Soon</Typography>
-    </Box>
-  );
+  redirect('/scenes');
 }
