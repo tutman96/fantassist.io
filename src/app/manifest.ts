ae37fc6d13a248bfbb28b8b810686134edd76b5c
tutman96 <11356668+tutman96@users.noreply.github.com>
2024-08-29 10:49:26 -0400

    dev specific title for pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,15 +1,19 @@
 import type { MetadataRoute } from "next";
 
+const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+
+const name = env === "local" ? `Fantassist - Dev` : "Fantassist";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Fantassist",
-    short_name: "Fantassist",
+    name,
+    short_name: name,
     description:
       "Fantassist is a web app built to allow creation and presentation of table-top roleplay map for in person sessions",
     start_url: "/",
     display: "standalone",
-    theme_color: '#071019', // theme.pallette.background.default
-    background_color: '#071019', // theme.pallette.background.default
+    theme_color: "#071019", // theme.pallette.background.default
+    background_color: "#071019", // theme.pallette.background.default
     icons: [
       {
         src: "/android-chrome-192x192.png",
